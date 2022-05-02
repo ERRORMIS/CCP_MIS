8ee2e1de18d75fe7b990b4b1bc555a79c4c0d500
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-05-02 13:32:29 +0530

    initial commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+const router = express.Router();
+
+import rateLimiter from "express-rate-limit";
+
+
+router.get('/',(req,res)=>{
+  res.json()
+})
+
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+import { register, login, updateUser } from "../controllers/authController.js";
+import authenticateUser from "../middleware/auth.js";
+router.route("/register").post(apiLimiter, register);
+router.route("/login").post(apiLimiter, login);
+router.route("/updateUser").patch(authenticateUser, updateUser);
+
+export default router;
